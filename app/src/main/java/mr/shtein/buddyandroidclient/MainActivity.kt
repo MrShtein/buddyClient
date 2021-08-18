e9a8139c34433b5f6d8e0416eb0ac868ffb264f6
Mr.Shtein <mr.shtein61@yandex.ru>
2021-08-18 19:58:31 +0300

    Init

---
+++ app/src/main/java/mr/shtein/buddyandroidclient/MainActivity.kt
@@ -0,0 +1,11 @@
+package mr.shtein.buddyandroidclient
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
