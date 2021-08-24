32f7efced4b82a16c4183ebff4a4e7b163d61614
Mr.Shtein <mr.shtein61@yandex.ru>
2021-08-24 19:55:18 +0300

    [feat] Добавлена анимция для перехода с заставки на экран приветствия (инструкции)

--- app/src/main/java/mr/shtein/buddyandroidclient/StartFragment.kt
+++ app/src/main/java/mr/shtein/buddyandroidclient/StartFragment.kt
@@ -1,15 +1,19 @@
 package mr.shtein.buddyandroidclient
 
+import android.animation.Animator
+import android.animation.AnimatorListenerAdapter
+import android.animation.AnimatorSet
+import android.animation.ObjectAnimator
 import android.os.Bundle
-import android.os.CountDownTimer
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
-import androidx.navigation.navOptions
 import com.airbnb.lottie.LottieAnimationView
-import java.util.*
+
+const val TAG = "StartFragment"
 
 class StartFragment : Fragment() {
 
@@ -25,28 +29,81 @@ class StartFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
 
-        val view = inflater.inflate(R.layout.start_fragment, container, false)
+        return inflater.inflate(R.layout.start_fragment, container, false)
 
-        return view
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        object : CountDownTimer(3000, 1000) {
-            override fun onTick(millisUntilFinished: Long) {
-                3 / 2
+        val sun: LottieAnimationView = view.findViewById(R.id.sun_pulse)
+
+        sun.setOnClickListener {
+
+            Log.d(TAG, "Click start")
+            val dog = view.findViewById<LottieAnimationView>(R.id.dog)
+            val cat = view.findViewById<LottieAnimationView>(R.id.cat)
+            val cloudTop = view.findViewById<LottieAnimationView>(R.id.cloud_top)
+            val cloudBottom = view.findViewById<LottieAnimationView>(R.id.cloud_bottom)
+
+            val dogOffset: Float = (dog.bottom - dog.top).toFloat()
+            val dogAnimator = ObjectAnimator.ofFloat(dog, "translationY", dogOffset).apply {
+                duration = 500
             }
-            override fun onFinish() {
-                findNavController().navigate(R.id.welcomeDescriptionFragment, null,
-                    navOptions { // Use the Kotlin DSL for building NavOptions
-                        anim {
-                            enter = android.R.anim.fade_in
-                            exit = android.R.anim.fade_out
-                        }
-                    })
+
+            val catOffset: Float = (cat.top - cat.bottom).toFloat()
+            val catAnimator = ObjectAnimator.ofFloat(cat, "translationY", catOffset).apply {
+                duration = 500
             }
+
+            val cloudTopAnimator = ObjectAnimator.ofFloat(cloudTop, "alpha",1f, 0f).apply {
+                duration = 500
+            }
+
+            val cloudBottomAnimator = ObjectAnimator.ofFloat(cloudBottom, "alpha",1f, 0f).apply {
+                duration = 500
+            }
+            val sun: LottieAnimationView = view.findViewById(R.id.sun_pulse)
+            val sunAnimatorX = ObjectAnimator.ofFloat(sun, "scaleX", 1f, 0f).apply {
+                duration = 500
+            }
+            val sunAnimatorY = ObjectAnimator.ofFloat(sun, "scaleY", 1f, 0f).apply {
+                duration = 500
+            }
+
+            AnimatorSet().apply {
+                addListener(object : AnimatorListenerAdapter() {
+                    override fun onAnimationEnd(animation: Animator?) {
+                        super.onAnimationEnd(animation)
+                        findNavController().navigate(R.id.welcomeDescriptionFragment)
+                    }
+                })
+                play(dogAnimator)
+                    .with(catAnimator)
+                    .with(cloudTopAnimator)
+                    .with(sunAnimatorX)
+                    .with(sunAnimatorY)
+                    .with(cloudBottomAnimator)
+                start()
+            }
+
+            Log.d(TAG, "Anim start")
         }
-            .start()
+
+//        object : CountDownTimer(3000, 1000) {
+//            override fun onTick(millisUntilFinished: Long) {
+//                3 / 2
+//            }
+//            override fun onFinish() {
+//                findNavController().navigate(R.id.welcomeDescriptionFragment, null,
+//                    navOptions { // Use the Kotlin DSL for building NavOptions
+//                        anim {
+//                            enter = android.R.anim.fade_in
+//                            exit = android.R.anim.fade_out
+//                        }
+//                    })
+//            }
+//        }
+//            .start()
 
     }
 
