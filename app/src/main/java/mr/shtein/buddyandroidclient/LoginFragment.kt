74b75f726c0bf152ea73b8ed8e6f6fb39ee108c7
Mr.Shtein <mr.shtein61@yandex.ru>
2021-10-04 15:25:21 +0300

    [feat] Добавил show/hide password

--- app/src/main/java/mr/shtein/buddyandroidclient/LoginFragment.kt
+++ app/src/main/java/mr/shtein/buddyandroidclient/LoginFragment.kt
@@ -1,10 +1,16 @@
 package mr.shtein.buddyandroidclient
 
 import android.os.Bundle
+import android.text.InputType
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ToggleButton
 
 class LoginFragment : Fragment() {
 
@@ -19,4 +25,16 @@ class LoginFragment : Fragment() {
         return inflater.inflate(R.layout.fragment_login, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val passwordVisibilityBtn = view.findViewById<ToggleButton>(R.id.password_visibility)
+        val passwordField = view.findViewById<EditText>(R.id.login_password_input_id)
+        passwordVisibilityBtn.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                passwordField.transformationMethod = HideReturnsTransformationMethod.getInstance()
+            } else {
+                passwordField.transformationMethod = PasswordTransformationMethod.getInstance()
+            }
+        }
+    }
 }
