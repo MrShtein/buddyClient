afe99a91497f23ce001583a3b4c71033a6d588b5
Mr.Shtein <mr.shtein61@yandex.ru>
2022-02-03 15:31:27 +0300

    [feat] Добавил подсказку на экране выбора города, и возможность перехода сразу на список животного, если город уже выбран

--- app/src/main/java/mr/shtein/buddyandroidclient/screens/SplashScreenFragment.kt
+++ app/src/main/java/mr/shtein/buddyandroidclient/screens/SplashScreenFragment.kt
@@ -1,11 +1,11 @@
 package mr.shtein.buddyandroidclient.screens
 
 import android.os.Bundle
-import android.os.CountDownTimer
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import mr.shtein.buddyandroidclient.R
+import mr.shtein.buddyandroidclient.utils.SharedPreferences
 
 
 class SplashScreenFragment : Fragment(R.layout.start_fragment) {
@@ -16,15 +16,15 @@ class SplashScreenFragment : Fragment(R.layout.start_fragment) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        object : CountDownTimer(3000, 1000) {
-            override fun onTick(millisUntilFinished: Long) {
-                3 / 2
-            }
+        val storage = SharedPreferences(requireContext(), SharedPreferences.PERSISTENT_STORAGE_NAME)
 
-            override fun onFinish() {
+        view.postDelayed({
+            if (storage.readString(SharedPreferences.USER_CITY_KEY, "") == "") {
                 findNavController().navigate(R.id.action_startFragment_to_cityChoiceFragment)
+            } else {
+                findNavController().navigate(R.id.action_startFragment_to_bottomNavFragment2)
             }
-        }
-            .start()
+        }, 3000)
+
     }
 }
