8afac961c36fb47582fbf3ab8191b2b3304d57bc
Mr.Shtein <mr.shtein61@yandex.ru>
2022-03-31 20:23:05 +0300

    [feat] Добавлено отображение информации о питомце, и счетчик фотографий

--- app/src/main/java/mr/shtein/buddyandroidclient/screens/kennels/AnimalSettingsFragment.kt
+++ app/src/main/java/mr/shtein/buddyandroidclient/screens/kennels/AnimalSettingsFragment.kt
@@ -6,15 +6,16 @@ import android.widget.ImageButton
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.*
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import mr.shtein.buddyandroidclient.R
 import mr.shtein.buddyandroidclient.adapters.AnimalPhotoAdapter
 import mr.shtein.buddyandroidclient.model.Animal
+import mr.shtein.buddyandroidclient.utils.OnSnapPositionChangeListener
+import mr.shtein.buddyandroidclient.utils.event.SnapOnScrollListener
 
 private const val ANIMAL_KEY = "animal_key"
 
-class AnimalSettingsFragment: Fragment(R.layout.animal_settings_fragment) {
+class AnimalSettingsFragment : Fragment(R.layout.animal_settings_fragment),
+    OnSnapPositionChangeListener {
     private lateinit var photoRecyclerView: RecyclerView
     private lateinit var layoutManager: LinearLayoutManager
     private lateinit var adapter: AnimalPhotoAdapter
@@ -25,6 +26,7 @@ class AnimalSettingsFragment: Fragment(R.layout.animal_settings_fragment) {
     private lateinit var gender: TextView
     private lateinit var description: TextView
     private lateinit var choiceBtn: ImageButton
+    private lateinit var photoCounter: TextView
     private var animal: Animal? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -49,12 +51,16 @@ class AnimalSettingsFragment: Fragment(R.layout.animal_settings_fragment) {
         gender = view.findViewById(R.id.animal_settings_gender)
         description = view.findViewById(R.id.animal_settings_description)
         choiceBtn = view.findViewById(R.id.animal_settings_change_btn)
+        photoCounter = view.findViewById(R.id.animal_settings_photo_counter)
     }
 
     private fun setDataToViews() {
         photoRecyclerView.setHasFixedSize(true)
         photoRecyclerView.layoutManager = layoutManager
         val snapHelper = PagerSnapHelper()
+        val snapOnScrollListener =
+            SnapOnScrollListener(snapHelper, this@AnimalSettingsFragment)
+        photoRecyclerView.addOnScrollListener(snapOnScrollListener)
         snapHelper.attachToRecyclerView(photoRecyclerView)
         val animalPhotoInString = animal?.imgUrl?.map {
             it.url
@@ -62,6 +68,32 @@ class AnimalSettingsFragment: Fragment(R.layout.animal_settings_fragment) {
         adapter = AnimalPhotoAdapter(animalPhotoInString ?: listOf())
         photoRecyclerView.adapter = adapter
 
+        photoCounter.text =
+            getString(
+                R.string.big_card_animal_photo_counter,
+                1,
+                animal?.imgUrl?.size ?: 1
+            )
 
+        animal?.let { animal ->
+            name.text = animal.name
+            breed.text = animal.breed
+            age.text = animal.getAge()
+            color.text = animal.characteristics["color"]
+            gender.text = animal.gender
+            description.text = animal.description
+        }
+
+
+    }
+
+    override fun onSnapPositionChange(position: Int) {
+        val elementsCount = animal?.imgUrl?.size
+        photoCounter.text =
+            getString(
+                R.string.big_card_animal_photo_counter,
+                position + 1,
+                elementsCount
+            )
     }
 }
