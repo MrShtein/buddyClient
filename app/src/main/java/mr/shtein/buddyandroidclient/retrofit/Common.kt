4afc2c4ba2899a908eef2c79914604e2fcfca7f6
Mr.Shtein <mr.shtein@outlook.com>
2022-06-29 18:02:31 +0300

    [feat] Добавлен адрес внешнего сервера

--- app/src/main/java/mr/shtein/buddyandroidclient/retrofit/Common.kt
+++ app/src/main/java/mr/shtein/buddyandroidclient/retrofit/Common.kt
@@ -1,7 +1,8 @@
 package mr.shtein.buddyandroidclient.retrofit
 
 object Common {
-    private const val BASE_URL = "http://10.0.2.2:8881/"
+    //private const val BASE_URL = "http://10.0.2.2:8881/"
+    private const val BASE_URL = "http://217.28.223.120:8881/"
     val retrofitService: RetrofitServices
         get() = RetrofitClient.getClient(BASE_URL).create(RetrofitServices::class.java)
 }
