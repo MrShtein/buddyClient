c5456c0bbed740dcb1444eadb525e7ec772ad00e
Mr.Shtein <mrshtein@outlook.com>
2022-07-17 13:21:58 +0300

    [feat] Создан репозиторий для работы с аутентификацией и авторизацией

---
+++ app/src/main/java/mr/shtein/buddyandroidclient/repository/AuthenticationRepository.kt
@@ -0,0 +1,18 @@
+package mr.shtein.buddyandroidclient.repository
+
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import mr.shtein.buddyandroidclient.retrofit.Common
+import mr.shtein.buddyandroidclient.retrofit.RetrofitServices
+import retrofit2.Response
+
+class AuthenticationRepository {
+
+    private val retrofit: RetrofitServices = Common.retrofitService
+
+    public suspend fun resetPassword(email: String) : Boolean = withContext(Dispatchers.IO) {
+        val result = retrofit.resetPassword(email)
+        return@withContext result.body() ?: false
+    }
+
+}
