9c2fb662be4d5e11fd1712ea57b0b70ae74f0cc7
Mr.Shtein <mrshtein@outlook.com>
2022-08-16 16:59:35 +0300

    [refactor] Добавлен динамический расчет нижнего margin в AnimalListFragment

--- app/src/main/java/mr/shtein/buddyandroidclient/presentation/screen/AnimalsListFragment.kt
+++ app/src/main/java/mr/shtein/buddyandroidclient/presentation/screen/AnimalsListFragment.kt
@@ -21,12 +21,10 @@ import androidx.navigation.NavDestination
 import androidx.navigation.fragment.findNavController
 import androidx.transition.Slide
 import com.google.android.material.transition.MaterialSharedAxis
-import mr.shtein.buddyandroidclient.BuddyApplication
-import mr.shtein.buddyandroidclient.R
+import mr.shtein.buddyandroidclient.*
 import mr.shtein.buddyandroidclient.adapters.OnAnimalCardClickListener
 import mr.shtein.buddyandroidclient.databinding.AnimalsListFragmentBinding
 import mr.shtein.buddyandroidclient.presentation.presenter.AnimalListPresenter
-import mr.shtein.buddyandroidclient.setStatusBarColor
 import mr.shtein.buddyandroidclient.utils.WorkFragment
 import org.koin.android.ext.android.inject
 
@@ -77,7 +75,7 @@ class AnimalsListFragment : Fragment(), OnAnimalCardClickListener, OnLocationBtn
         _binding = AnimalsListFragmentBinding.inflate(inflater, container, false)
         val view = binding.root
         animalListPresenter.onAttachView(this)
-
+        changeMarginBottom(binding.animalsListSwipeLayout, requireActivity() as MainActivity)
         setStatusBarColor(true)
         setInsetsListener(binding.animalChoiceChips)
         initRecyclerView()
