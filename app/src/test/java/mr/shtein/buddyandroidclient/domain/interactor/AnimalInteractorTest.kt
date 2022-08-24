716d3372dd0089aabbfb5d1ac96fc37676d497ed
Mr.Shtein <mrshtein@outlook.com>
2022-08-24 16:44:26 +0300

    [test] Добавлена kotlin библиотека для тестирования suspend функций и изменен тест AnimalInteractorTest

--- app/src/test/java/mr/shtein/buddyandroidclient/domain/interactor/AnimalInteractorTest.kt
+++ app/src/test/java/mr/shtein/buddyandroidclient/domain/interactor/AnimalInteractorTest.kt
@@ -1,9 +1,9 @@
+@file:OptIn(ExperimentalCoroutinesApi::class)
 package mr.shtein.buddyandroidclient.domain.interactor
 
 import io.github.serpro69.kfaker.Faker
-import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.withContext
 import mr.shtein.buddyandroidclient.data.repository.AnimalRepository
 import mr.shtein.buddyandroidclient.data.repository.DistanceCounterRepository
 import mr.shtein.buddyandroidclient.model.Animal
@@ -25,12 +25,8 @@ class AnimalInteractorTest {
         val testAnimalList: List<Animal> = List(50) {
             faker.randomProvider.randomClassInstance()
         }
+        Mockito.`when`(animalRepository.getAnimals(listOf(1,2))).thenReturn(testAnimalList)
 
-        animalRepository.stub {
-            onBlocking {
-                getAnimals(listOf(1,2))
-            }.thenReturn(testAnimalList)
-        }
         val animalInteractor: AnimalInteractor = AnimalInteractorImpl(
             animalRepository, networkDistanceCounterRepository
         )
