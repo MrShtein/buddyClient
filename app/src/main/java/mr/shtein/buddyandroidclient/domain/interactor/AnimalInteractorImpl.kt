f8336d2d30bf8d19435733786c901cceab343415
Mr.Shtein <mrshtein@outlook.com>
2022-09-07 18:10:52 +0300

    [feat] Добавлены аннотации @Throws для упрощения тестирования

--- app/src/main/java/mr/shtein/buddyandroidclient/domain/interactor/AnimalInteractorImpl.kt
+++ app/src/main/java/mr/shtein/buddyandroidclient/domain/interactor/AnimalInteractorImpl.kt
@@ -2,12 +2,17 @@ package mr.shtein.buddyandroidclient.domain.interactor
 
 import mr.shtein.buddyandroidclient.data.repository.AnimalRepository
 import mr.shtein.buddyandroidclient.data.repository.DistanceCounterRepository
+import mr.shtein.buddyandroidclient.exceptions.validate.ServerErrorException
 import mr.shtein.buddyandroidclient.model.Animal
 import mr.shtein.buddyandroidclient.model.Coordinates
 import org.koin.core.component.KoinComponent
 import org.koin.core.component.inject
+import java.net.ConnectException
+import java.net.SocketTimeoutException
+import kotlin.jvm.Throws
 
 interface AnimalInteractor {
+    @Throws(ConnectException::class, SocketTimeoutException::class, ServerErrorException::class)
     suspend fun getAnimalsByFilter(animalFilter: List<Int>): List<Animal>
     suspend fun getDistancesFromUser(token: String, coordinates: Coordinates): HashMap<Int, Int>
 }
@@ -16,7 +21,6 @@ class AnimalInteractorImpl(
     private val animalRepository: AnimalRepository,
     private val retrofitDistanceCounterRepository: DistanceCounterRepository
 ) : AnimalInteractor {
-
     override suspend fun getAnimalsByFilter(animalFilter: List<Int>): List<Animal> {
         return animalRepository.getAnimals(animalFilter)
     }
