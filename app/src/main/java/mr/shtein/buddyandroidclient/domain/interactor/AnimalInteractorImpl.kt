2fb40c2f7080294cda19593e7f37fc7f6fbbc3e2
Mr.Shtein <mrshtein@outlook.com>
2022-09-15 16:35:04 +0300

    [feat] Добавлен репозиторий для работы с типами питомцев

--- app/src/main/java/mr/shtein/buddyandroidclient/domain/interactor/AnimalInteractorImpl.kt
+++ app/src/main/java/mr/shtein/buddyandroidclient/domain/interactor/AnimalInteractorImpl.kt
@@ -3,13 +3,13 @@ package mr.shtein.buddyandroidclient.domain.interactor
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import mr.shtein.buddyandroidclient.data.repository.AnimalRepository
+import mr.shtein.buddyandroidclient.data.repository.AnimalTypeRepository
 import mr.shtein.buddyandroidclient.data.repository.DistanceCounterRepository
 import mr.shtein.buddyandroidclient.exceptions.validate.ServerErrorException
 import mr.shtein.buddyandroidclient.model.Animal
 import mr.shtein.buddyandroidclient.model.Coordinates
 import mr.shtein.buddyandroidclient.model.dto.AnimalFilter
-import org.koin.core.component.KoinComponent
-import org.koin.core.component.inject
+import mr.shtein.buddyandroidclient.model.dto.AnimalType
 import java.net.ConnectException
 import java.net.SocketTimeoutException
 import kotlin.jvm.Throws
@@ -18,11 +18,13 @@ interface AnimalInteractor {
     @Throws(ConnectException::class, SocketTimeoutException::class, ServerErrorException::class)
     suspend fun getAnimalsByFilter(animalFilter: AnimalFilter): List<Animal>
     suspend fun getDistancesFromUser(token: String, coordinates: Coordinates): HashMap<Int, Int>
+    suspend fun getAnimalTypes(): List<AnimalType>
 }
 
 class AnimalInteractorImpl(
     private val animalRepository: AnimalRepository,
-    private val retrofitDistanceCounterRepository: DistanceCounterRepository
+    private val retrofitDistanceCounterRepository: DistanceCounterRepository,
+    private val animalTypeRepository: AnimalTypeRepository
 ) : AnimalInteractor {
     override suspend fun getAnimalsByFilter(animalFilter: AnimalFilter): List<Animal> =
         withContext(Dispatchers.IO) {
@@ -35,4 +37,9 @@ class AnimalInteractorImpl(
     ): HashMap<Int, Int> {
         return retrofitDistanceCounterRepository.getDistancesFromUser(token, coordinates)
     }
+
+    override suspend fun getAnimalTypes(): List<AnimalType> {
+        return animalTypeRepository.getAnimalTypes()
+    }
+
 }
--- app/src/main/java/mr/shtein/buddyandroidclient/model/AnimalType.kt
+++
@@ -1,7 +0,0 @@
-package mr.shtein.buddyandroidclient.model
-
-data class AnimalType(
-    val id: Int,
-    val animalType: String,
-    val pluralAnimalType: String
-    )
