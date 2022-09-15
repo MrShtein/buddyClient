29ff51b93a9617b802704dc3dbcafe375ce97500
Mr.Shtein <mrshtein@outlook.com>
2022-09-15 18:38:29 +0300

    [feat] В AnimalInteractor добавлена логика работы с породами, характеристиками и типами животных

--- app/src/main/java/mr/shtein/buddyandroidclient/domain/interactor/AnimalInteractorImpl.kt
+++ app/src/main/java/mr/shtein/buddyandroidclient/domain/interactor/AnimalInteractorImpl.kt
@@ -2,27 +2,35 @@ package mr.shtein.buddyandroidclient.domain.interactor
 
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
-import mr.shtein.buddyandroidclient.data.repository.AnimalRepository
-import mr.shtein.buddyandroidclient.data.repository.AnimalTypeRepository
-import mr.shtein.buddyandroidclient.data.repository.DistanceCounterRepository
+import mr.shtein.buddyandroidclient.data.repository.*
+import mr.shtein.buddyandroidclient.exceptions.validate.IncorrectDataException
 import mr.shtein.buddyandroidclient.exceptions.validate.ServerErrorException
 import mr.shtein.buddyandroidclient.model.Animal
 import mr.shtein.buddyandroidclient.model.Coordinates
+import mr.shtein.buddyandroidclient.model.dto.AnimalCharacteristic
 import mr.shtein.buddyandroidclient.model.dto.AnimalFilter
 import mr.shtein.buddyandroidclient.model.dto.AnimalType
+import mr.shtein.buddyandroidclient.model.dto.Breed
 import java.net.ConnectException
 import java.net.SocketTimeoutException
 import kotlin.jvm.Throws
+
+private const val COLOR_ID = 1
 
 interface AnimalInteractor {
     @Throws(ConnectException::class, SocketTimeoutException::class, ServerErrorException::class)
     suspend fun getAnimalsByFilter(animalFilter: AnimalFilter): List<Animal>
     suspend fun getDistancesFromUser(token: String, coordinates: Coordinates): HashMap<Int, Int>
     suspend fun getAnimalTypes(): List<AnimalType>
+    suspend fun getAnimalBreeds(animalTypeId: Int): List<Breed>
+    suspend fun getAnimalCharacteristics(characteristicId: Int): List<AnimalCharacteristic>
 }
 
 class AnimalInteractorImpl(
     private val animalRepository: AnimalRepository,
+    private val animalBreedRepository: AnimalBreedRepository,
+    private val sharedUserPropertiesRepository: SharedUserPropertiesRepository,
+    private val animalCharacteristicsRepository: AnimalCharacteristicsRepository,
     private val retrofitDistanceCounterRepository: DistanceCounterRepository,
     private val animalTypeRepository: AnimalTypeRepository
 ) : AnimalInteractor {
@@ -42,4 +50,24 @@ class AnimalInteractorImpl(
         return animalTypeRepository.getAnimalTypes()
     }
 
+    override suspend fun getAnimalBreeds(animalTypeId: Int): List<Breed> {
+        val token = getUserToken()
+        return animalBreedRepository.getAnimalBreeds(token = token, animalTypeId = animalTypeId)
+    }
+
+    override suspend fun getAnimalCharacteristics(characteristicId: Int): List<AnimalCharacteristic> {
+        val token = getUserToken()
+        return when(characteristicId) {
+            COLOR_ID -> {
+               animalCharacteristicsRepository.getAnimalColors(token)
+            }
+            else -> {
+                throw IncorrectDataException()
+            }
+        }
+    }
+
+    private fun getUserToken(): String {
+        return sharedUserPropertiesRepository.getUserToken()
+    }
 }
