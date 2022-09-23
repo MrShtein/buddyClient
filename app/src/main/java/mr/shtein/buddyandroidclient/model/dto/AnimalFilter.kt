574b8a8b66e256c338ab83efcbe9551e112c86be
Mr.Shtein <mrshtein@outlook.com>
2022-09-23 18:22:05 +0300

    [fix] Изменены типы переменных

--- app/src/main/java/mr/shtein/buddyandroidclient/model/dto/AnimalFilter.kt
+++ app/src/main/java/mr/shtein/buddyandroidclient/model/dto/AnimalFilter.kt
@@ -6,10 +6,10 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class AnimalFilter(
     var animalTypeId: MutableList<Int>? = null,
-    val cityId: List<Int>? = null,
-    val breedId: List<Int>? = null,
-    val characteristicId: List<Int>? = null,
-    val genderId: List<Int>? = null,
+    val cityId: MutableList<Int>? = null,
+    val breedId: MutableList<Int>? = null,
+    val characteristicId: MutableList<Int>? = null,
+    val genderId: MutableList<Int>? = null,
     val minAge: Int = -1,
     val maxAge: Int = -1
 ) : Parcelable
