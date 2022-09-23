0cf806b079cf2fb70f9434ada02c9c10a405731c
Mr.Shtein <mrshtein@outlook.com>
2022-09-23 18:13:53 +0300

    [feat] В интерактор и репозиторий добавлены методы для получения пород, типов питомцев, характеристик и кол-ва найденных животных

--- app/src/main/java/mr/shtein/buddyandroidclient/domain/interactor/AnimalInteractorImpl.kt
+++ app/src/main/java/mr/shtein/buddyandroidclient/domain/interactor/AnimalInteractorImpl.kt
@@ -18,9 +18,10 @@ interface AnimalInteractor {
     @Throws(ConnectException::class, SocketTimeoutException::class, ServerErrorException::class)
     suspend fun getAnimalsByFilter(animalFilter: AnimalFilter): List<Animal>
     suspend fun getDistancesFromUser(token: String, coordinates: Coordinates): HashMap<Int, Int>
-    suspend fun getAnimalTypes(): List<AnimalType>
-    suspend fun getAnimalBreeds(animalTypeId: Int): List<FilterAutocompleteItem>
-    suspend fun getAnimalCharacteristics(characteristicId: Int): List<AnimalCharacteristic>
+    suspend fun getAnimalTypes(): List<FilterAutocompleteItem>
+    suspend fun getAnimalBreeds(animalTypeList: List<Int>): List<FilterAutocompleteItem>
+    suspend fun getAnimalCharacteristics(characteristicId: Int): List<FilterAutocompleteItem>
+    suspend fun getAnimalsCountByFilter(animalFilter: AnimalFilter): Int
 }
 
 class AnimalInteractorImpl(
@@ -43,27 +44,58 @@ class AnimalInteractorImpl(
         return retrofitDistanceCounterRepository.getDistancesFromUser(token, coordinates)
     }
 
-    override suspend fun getAnimalTypes(): List<AnimalType> {
-        return animalTypeRepository.getAnimalTypes()
+    override suspend fun getAnimalTypes(): List<FilterAutocompleteItem> {
+        val animalType = animalTypeRepository.getAnimalTypes()
+        return mapAnimalTypeToFilterItem(animalType)
     }
 
-    override suspend fun getAnimalBreeds(animalTypeId: Int): List<FilterAutocompleteItem> {
+    override suspend fun getAnimalBreeds(animalTypeList: List<Int>): List<FilterAutocompleteItem> {
         val token = getUserToken()
-        val animalBreeds =
-            animalBreedRepository.getAnimalBreeds(token = token, animalTypeId = animalTypeId)
-        return mapBreedsToFilterBreeds(breeds = animalBreeds)
+        val allBreeds = mutableListOf<Breed>()
+        animalTypeList.forEach {
+            val animalBreeds =
+                animalBreedRepository.getAnimalBreeds(token = token, animalTypeId = it)
+            allBreeds.addAll(animalBreeds)
+        }
+        return mapBreedsToFilterBreeds(breeds = allBreeds)
     }
 
-    override suspend fun getAnimalCharacteristics(characteristicId: Int): List<AnimalCharacteristic> {
+    override suspend fun getAnimalCharacteristics(characteristicId: Int): List<FilterAutocompleteItem> {
         val token = getUserToken()
         return when (characteristicId) {
             COLOR_ID -> {
-                animalCharacteristicsRepository.getAnimalColors(token)
+                val animalColors = animalCharacteristicsRepository.getAnimalColors(token)
+                mapCharacteristicToFilterItem(animalColors)
             }
             else -> {
                 throw IncorrectDataException()
             }
         }
+    }
+
+    override suspend fun getAnimalsCountByFilter(animalFilter: AnimalFilter): Int =
+        withContext(Dispatchers.IO) {
+            return@withContext animalRepository.getAnimalsCountByFilter(animalFilter)
+        }
+
+    private fun mapAnimalTypeToFilterItem(
+        animalType: List<AnimalType>
+    ): List<FilterAutocompleteItem> {
+        return animalType
+            .map {
+                FilterAutocompleteItem(id = it.id, name = it.pluralAnimalType, isSelected = false)
+            }
+            .toList()
+    }
+
+    private fun mapCharacteristicToFilterItem(
+        animalColors: List<AnimalCharacteristic>
+    ): List<FilterAutocompleteItem> {
+        return animalColors
+            .map {
+                FilterAutocompleteItem(id = it.id, name = it.name, isSelected = false)
+            }
+            .toList()
     }
 
     private fun getUserToken(): String {
@@ -73,12 +105,9 @@ class AnimalInteractorImpl(
     private fun mapBreedsToFilterBreeds(breeds: List<Breed>): List<FilterAutocompleteItem> {
         return breeds
             .map {
-                FilterAutocompleteItem(
-                    it.id,
-                    it.name,
-                    false
-                )
+                FilterAutocompleteItem(id = it.id, name = it.name, isSelected = false)
             }
             .toList()
     }
+
 }
