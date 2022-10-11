3c45d391c3c32cd32a2171a6e802262ba211949d
Mr.Shtein <mrshtein@outlook.com>
2022-10-11 19:50:42 +0300

    [feat] Добавлены изменения для huawei

--- app/src/huawei/java/mr/shtein/buddyandroidclient/di/module/InteractorModule.kt
+++ app/src/huawei/java/mr/shtein/buddyandroidclient/di/module/InteractorModule.kt
@@ -1,16 +1,14 @@
 package mr.shtein.buddyandroidclient.di.module
 
 import com.huawei.hms.location.LocationRequest
-import mr.shtein.buddyandroidclient.domain.interactor.AnimalInteractor
-import mr.shtein.buddyandroidclient.domain.interactor.AnimalInteractorImpl
-import mr.shtein.buddyandroidclient.domain.interactor.LocationInteractor
-import mr.shtein.buddyandroidclient.domain.interactor.LocationServiceInteractor
+import mr.shtein.buddyandroidclient.domain.interactor.*
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 val interactorModule: Module = module {
-    single<AnimalInteractor> { AnimalInteractorImpl(get(), get()) }
+    single<AnimalInteractor> { AnimalInteractorImpl(get(), get(), get(), get(), get(), get()) }
+    single<AnimalFilterInteractor> { AnimalFilterInteractorImpl(get()) }
     single<LocationInteractor> { LocationServiceInteractor(get(), get()) }
 
 }
