c1ee942e652f111322a511633867d96e43babc6b
MrShtein <mr.shtein@outlook.com>
2022-12-28 19:02:12 +0300

    [refactor] Удален неиспользуемое состояние

--- feature/kennel/src/main/java/mr/shtein/kennel/presentation/state/kennel_settings/CityFieldState.kt
+++ feature/kennel/src/main/java/mr/shtein/kennel/presentation/state/kennel_settings/CityFieldState.kt
@@ -3,7 +3,6 @@ package mr.shtein.kennel.presentation.state.kennel_settings
 import mr.shtein.kennel.CityField
 
 sealed class CityFieldState {
-    object Validate : CityFieldState()
     data class Value(val value: CityField) : CityFieldState()
     data class Error(val message: String) : CityFieldState()
 }
