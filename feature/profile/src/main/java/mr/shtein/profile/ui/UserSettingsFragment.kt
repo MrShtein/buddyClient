f3ea65bc36eef05f5b21766ed4ca89a04d9ebdb0
MrShtein <mr.shtein@outlook.com>
2023-01-03 15:06:27 +0300

    [fix] Убран двойной фон на уведомлении при изменении настроек пользователя (issue #16)

--- feature/profile/src/main/java/mr/shtein/profile/ui/UserSettingsFragment.kt
+++ feature/profile/src/main/java/mr/shtein/profile/ui/UserSettingsFragment.kt
@@ -464,7 +464,6 @@ class UserSettingsFragment : Fragment(R.layout.user_settings_fragment) {
         dialog = MaterialAlertDialogBuilder(requireContext(), R.style.MyDialog)
 
             .setView(R.layout.user_settings_dialog)
-            //.setBackground(ColorDrawable(requireContext().getColor(R.color.transparent)))
             .show()
 
         val positiveDialogBtn: Button? = dialog.findViewById(R.id.user_settings_dialog_positive_btn)
