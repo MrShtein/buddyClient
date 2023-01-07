617724bef973c6158546faa468829ff99505a133
MrShtein <mr.shtein@outlook.com>
2023-01-07 16:36:42 +0300

    [refactor] Добавлена общая viewmodel для хранения общих свойств для приложения

--- util/src/main/java/mr/shtein/util/di/ValidatorModule.kt
+++ util/src/main/java/mr/shtein/util/di/ValidatorModule.kt
@@ -2,12 +2,15 @@ package mr.shtein.util.di
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
+import mr.shtein.util.CommonViewModel
 import mr.shtein.util.validator.PasswordValidator
+import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.core.module.Module
 import org.koin.dsl.module
 
 val validatorModule: Module = module {
     factory { PasswordValidator(get(), provideCoroutineScope()) }
+    viewModel { CommonViewModel() }
 }
 
 private fun provideCoroutineScope() = CoroutineScope(Dispatchers.Main)
