d2145e33526fe018aeabb965fb4cfcc5c47514bb
MrShtein <mr.shtein@outlook.com>
2023-02-23 14:37:24 +0300

    [feat] Добавлен timeout для медленных соединений

--- core/network/src/main/java/mr/shtein/network/di/NetworkModule.kt
+++ core/network/src/main/java/mr/shtein/network/di/NetworkModule.kt
@@ -5,11 +5,13 @@ import mr.shtein.network.BuildConfig
 import mr.shtein.network.ImageLoader
 import mr.shtein.network.NetworkImageLoader
 import mr.shtein.network.NetworkService
+import okhttp3.OkHttpClient
 import org.koin.core.module.Module
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.converter.scalars.ScalarsConverterFactory
+import java.util.concurrent.TimeUnit
 
 val networkModule: Module = module {
     single { provideRetrofit() }
@@ -17,9 +19,14 @@ val networkModule: Module = module {
     single { provideNetworkImageLoader() }
 }
 
-fun provideRetrofit(): Retrofit =
-    Retrofit.Builder()
-        .baseUrl(BuildConfig.HOST)
+fun provideRetrofit(): Retrofit {
+    val okHttpClient: OkHttpClient = OkHttpClient.Builder()
+        .readTimeout(BuildConfig.NETWORK_TIMEOUT, TimeUnit.MILLISECONDS)
+        .connectTimeout(BuildConfig.NETWORK_TIMEOUT, TimeUnit.MILLISECONDS)
+        .build()
+
+    return Retrofit.Builder()
+    .baseUrl(BuildConfig.HOST)
         .addConverterFactory(ScalarsConverterFactory.create())
         .addConverterFactory(
             GsonConverterFactory.create(
@@ -28,7 +35,10 @@ fun provideRetrofit(): Retrofit =
                     .create()
             )
         )
+        .client(okHttpClient)
         .build()
+}
+
 
 fun provideNetworkService(retrofit: Retrofit): NetworkService =
      retrofit.create(NetworkService::class.java)
